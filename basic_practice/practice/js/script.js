ed5b18ed63fbca2b8da89e38d195eb357289a215
今村萌香 <imamuramoeka@imamuramoekanoMacBook-Pro.local>
2020-09-25 16:14:31 +0900

    スライドショー

---
+++ basic_practice/practice/js/script.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+    $(".btn").on("click",()=>{
+    $("#slide").slideToggle();
+   })
+    
+  $("#slide").slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots:true
+    });
+
+});
